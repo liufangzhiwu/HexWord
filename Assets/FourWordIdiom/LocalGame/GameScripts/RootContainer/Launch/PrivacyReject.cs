62008233b28ade73ed353fe2221aa6594c5c669e
fangxiujing <2608544640@qq.com>
2025-12-12 15:11:55 +0800

    11

---
+++ Assets/FourWordIdiom/LocalGame/GameScripts/RootContainer/Launch/PrivacyReject.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrivacyReject : MonoBehaviour
+{
+    [SerializeField] private HyperlinkText _descriptionText;
+    [SerializeField] private Button _callbackButton;
+    [SerializeField] private Button _rejectButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _descriptionText.text = MultilingualManager.Instance.GetString("PrivacyAgreement04");
+        _descriptionText.onHyperlinkClick = OnClickText;
+        _callbackButton.AddClickAction(OnCallbackClick);
+        _rejectButton.AddClickAction(OnRejectClick);
+    }
+
+    private void OnRejectClick()
+    {
+        Application.Quit();
+    }
+
+    private void OnCallbackClick()
+    {
+        gameObject.SetActive(false);
+        transform.parent.GetComponentInChildren<PrivacyGuidance>(true).gameObject.SetActive(true);
+    }
+    
+    void OnClickText(string url)
+    {
+        Debug.Log("点击"+url);
+        Application.OpenURL(url);
+    }
+}
