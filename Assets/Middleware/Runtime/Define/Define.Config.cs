1be87138a40bbccfdba4f501232387d0959f2c11
zjs <mirozeng2023@gmail.com>
2026-01-10 18:54:26 +0800

    修复安卓sdk问题

--- Assets/Middleware/Runtime/Define/Define.Config.cs
+++ Assets/Middleware/Runtime/Define/Define.Config.cs
@@ -18,8 +18,8 @@ namespace Middleware
         public struct ConfigHuaweiAndroid
         {
             public const string TestBannerAdId = "ca-app-pub-3940256099942544/6300978111";
-            public const string TestInterstitialAdId = "ca-app-pub-3940256099942544/1033173712";
-            public const string TestRewardAdId = "ca-app-pub-3940256099942544/5224354917";
+            public const string TestInterstitialAdId = "teste9ih9j0rc3";
+            public const string TestRewardAdId = "u2k89ub7vq";
         }
 #endif
 
